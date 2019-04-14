b72e5c1381042f7c95fd2a79758755afefb5143c
Paul-Sebastian Manole <brokenthorn@gmail.com>
2019-04-14 22:54:35 +0300

    Remove scripts that shouldn't be here

--- meal_tickets_tracker/create_function_trim_month_column_to_month.pgsql.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION trim_month_column_to_month() RETURNS TRIGGER AS
-$$
-BEGIN
-  new.month := date_trunc('month', new.month);
-  RETURN new;
-END;
-$$ LANGUAGE plpgsql;
--- meal_tickets_tracker/populate_table_employee.pgsql.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO employee (first_name, last_name, cnp)
-VALUES ('Paul-Sebastian', 'Manole', '1880115142615');
-
-INSERT INTO employee (first_name, last_name, cnp)
-VALUES ('Valentina', 'Călin', '2690315360021');
