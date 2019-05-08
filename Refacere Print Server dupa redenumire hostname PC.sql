7fe6c84c9cadc6fb5da3405d72d7581ae2686a9f
Paul-Sebastian Manole <brokenthorn@gmail.com>
2019-05-08 15:55:35 +0300

    New scripts

---
+++ Refacere Print Server dupa redenumire hostname PC.sql
@@ -0,0 +1,42 @@
+SELECT *
+FROM CasaMarcatXPrintServer;
+
+SELECT *
+FROM CasaMarcatStatieXPrintServer;
+
+SELECT *
+FROM ImprimantaXPrintServer;
+
+SELECT *
+FROM ImprimantaStatieXPrintServer;
+
+-- 1. Check previous values with above SELECTs
+-- 2. Run below transaction but don't COMMIT
+-- 3. Check new values with above SELECTSs.
+--    Make sure only the rows that should have changed, changed.
+-- 4. COMMIT or ROLLBACK depending on results.
+
+BEGIN TRANSACTION
+    DECLARE
+        @DenumireVechePrintServer VARCHAR(50) = '',
+        @DenumireNouaPrintServer  VARCHAR(50) = '';
+
+    UPDATE CasaMarcatXPrintServer
+    SET PrintServer = @DenumireNouaPrintServer
+    WHERE PrintServer = @DenumireVechePrintServer;
+
+    UPDATE CasaMarcatStatieXPrintServer
+    SET Statie = @DenumireNouaPrintServer
+    WHERE Statie = @DenumireVechePrintServer;
+
+    UPDATE ImprimantaXPrintServer
+    SET PrintServer = @DenumireNouaPrintServer
+    WHERE PrintServer = @DenumireVechePrintServer;
+
+    UPDATE ImprimantaStatieXPrintServer
+    SET Statie = @DenumireNouaPrintServer
+    WHERE Statie = @DenumireVechePrintServer;
+
+-- ROLLBACK
+
+-- COMMIT
