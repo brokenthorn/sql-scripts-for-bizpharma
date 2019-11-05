807cab41b398f6ee8533092ce46b31aebffd8299
Paul-Sebastian Manole <brokenthorn@gmail.com>
2019-11-05 17:02:50 +0200

    Update Carduri definitie rapida discount pe combinatii de bon x tip articol x categorie articol.sql

--- Carduri definitie rapida discount pe combinatii de bon x tip articol x categorie articol.sql
+++ Carduri definitie rapida discount pe combinatii de bon x tip articol x categorie articol.sql
@@ -1,3 +1,5 @@
+-- Script de setare automata configuratii Card pe varianta CarduriXBonDefinitieXCategorieArticolXTipArticol
+
 -- Previzualizare definitie la nivel de bon definitie x categorie articol x tip articol:
 -- SELECT *
 -- FROM CarduriXBonDefinitieXCategorieArticolXTipArticol
@@ -7,7 +9,9 @@ DECLARE @numeSerie VARCHAR(50) = 'Regina Maria';
 DECLARE @procDisc DECIMAL(6, 3) = 18.0;
 DECLARE @seExcludBonurileDecontateUM BIT = 1;
 
--- bon definitie (sau tip bon) pentru toate retetele, fara la liber (Normal):
+-- restul filtrelor sunt de tip tabel si se compun dupa cum urmeaza:
+
+-- filtru: bon definitie (sau tip bon):
 DECLARE @bonDefinitii TABLE
                       (
                           IdBonDefinitie INT NOT NULL PRIMARY KEY,
@@ -19,7 +23,7 @@ SELECT IdBonDefinitie
 FROM BonDefinitie
 WHERE Nume NOT LIKE 'Normal';
 
--- categoria de articol Rx:
+-- filtru: categoria de articol:
 DECLARE @categorii TABLE
                    (
                        IdCategorie INT NOT NULL PRIMARY KEY,
@@ -33,39 +37,41 @@ WHERE IdDictionar = 7
   AND EsteActiv = 1
   AND Nume LIKE 'Rx';
 
--- tipuri de articol active pentru categoriile de articol definite:
+-- filtru: tipuri de articol:
 DECLARE @tipuriArticol TABLE
                        (
                            IdTipArticol INT NOT NULL PRIMARY KEY,
                            procesat     BIT NOT NULL DEFAULT 0
                        );
 
+-- setam la toate tipurile de articol utilizate actualmente
+-- pentru categoriile de articol introduse mai sus in @categorii:
 INSERT INTO @tipuriArticol (IdTipArticol)
 SELECT DISTINCT IdTipArticol
 FROM Articol
 WHERE IdCategorie IN (SELECT IdCategorie FROM @categorii)
   AND AreStoc = 1;
 
-
+-- running variables:
 DECLARE @idCat INT;
 DECLARE @idTip INT;
 DECLARE @idBonDef INT;
 
--- categorii
+-- loop categorii
 WHILE (SELECT COUNT(*)
        FROM @categorii
        WHERE procesat = 0) > 0
     BEGIN
         SELECT TOP 1 @idCat = IdCategorie FROM @categorii WHERE procesat = 0;
 
-        -- tipuri articol:
+        -- loop tipuri articol:
         WHILE (SELECT COUNT(*)
                FROM @tipuriArticol
                WHERE procesat = 0) > 0
             BEGIN
                 SELECT TOP 1 @idTip = IdTipArticol FROM @tipuriArticol WHERE procesat = 0;
 
-                -- bon definitii:
+                -- loop bon definitii:
                 WHILE (SELECT COUNT(*)
                        FROM @bonDefinitii
                        WHERE procesat = 0) > 0
@@ -80,12 +86,14 @@ WHILE (SELECT COUNT(*)
 
                         UPDATE @bonDefinitii SET procesat = 1 WHERE IdBonDefinitie = @idBonDef;
                     END
+
                 -- s-au procesat toate definitiile de bon pentru aceasta runda din loop-ul tip articol,
                 -- deci resetam bitul:
                 UPDATE @bonDefinitii SET procesat = 0 WHERE procesat = 1;
 
                 UPDATE @tipuriArticol SET procesat = 1 WHERE IdTipArticol = @idTip;
             END
+
         -- s-au procesat toate tipurile de articol pentru aceasta runda din loop-ul categorii articol,
         -- deci resetam bitul:
         UPDATE @tipuriArticol SET procesat = 0 WHERE procesat = 1;
