7d3e9e58b8cbd93c530762b5dc4f32ff2206edbe
Paul-Sebastian Manole <brokenthorn@gmail.com>
2020-03-06 16:53:20 +0200

    Fix wrong field in calculation

--- nomenclatoare/Articole cu Producator si Stoc actual.sql
+++ nomenclatoare/Articole cu Producator si Stoc actual.sql
@@ -2,12 +2,12 @@ SELECT A.IdArticol,
        A.Cod,
        A.Nume                                          AS Nume,
        Prod.Nume                                       AS Producator,
-       Prds.CantNrBuc                                  AS Imp,
+       Stoc.CantNrBuc                                  AS ImpStoc,
        SUM(Stoc.CantI) +
        FLOOR(
-               sum(stoc.CantF) / CAST(Prds.CantNrBuc AS MONEY)
+               sum(Stoc.CantF) / CAST(Stoc.CantNrBuc AS MONEY)
            )                                           AS T_CantI,
-       SUM(stoc.CantF) % CAST(Prds.CantNrBuc AS MONEY) AS T_CantF
+       SUM(Stoc.CantF) % CAST(Stoc.CantNrBuc AS MONEY) AS T_CantF
 FROM Articol A
          LEFT JOIN Produs Prds ON A.IdArticol = Prds.IdArticol
          LEFT JOIN Partener Prod ON Prod.IdPartener = Prds.IdProducator
@@ -17,4 +17,5 @@ GROUP BY A.IdArticol,
          A.Cod,
          A.Nume,
          Prod.Nume,
-         Prds.CantNrBuc
+         Stoc.CantNrBuc;
+GO
