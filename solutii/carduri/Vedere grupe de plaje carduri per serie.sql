7a5c5837e11ebd4e16c88b4eea75e8733a6d1fa6
Paul-Sebastian Manole <brokenthorn@gmail.com>
2020-06-22 10:35:30 +0300

    Update Vedere grupe de plaje carduri per serie.sql

--- solutii/carduri/Vedere grupe de plaje carduri per serie.sql
+++ solutii/carduri/Vedere grupe de plaje carduri per serie.sql
@@ -7,3 +7,11 @@ FROM Carduri
 WHERE ISNUMERIC(NumarCard) = 1
 GROUP BY NumeSerie, LEFT(NumarCard, 1)
 ORDER BY min(NumarCard) ASC
+
+
+-- 100000083101
+SELECT NumeSerie, NumarCard
+FROM Carduri
+WHERE NumarCard >= '100000083101' and NumarCard <= '2000000000000'
+  AND ISNUMERIC(NumarCard) = 1
+  AND NumeSerie = 'Mini-Farm Fidelizare'
